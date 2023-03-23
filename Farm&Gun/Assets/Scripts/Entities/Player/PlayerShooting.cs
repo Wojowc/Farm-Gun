3dff7319416e28103a5a0f8292e5c5a805c4ae0c
Biurito <oto.wojtek@gmail.com>
2023-03-23 11:37:38 +0100

    Fire

---
+++ Farm&Gun/Assets/Scripts/Entities/Player/PlayerShooting.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject projectile;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Fire!");
+        }
+    }
+}
