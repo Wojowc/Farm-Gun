2f22a5ccc893751b33b308eb9ecdbefdf699e838
kochamdziekana <czechumennakompie@gmail.com>
2023-03-23 19:01:05 +0100

    Added Prototypes, PrototypeGenerators (not all prototypes added, constraints are absent), implemented WFC.

---
+++ Farm&Gun/Assets/Scripts/DataGenerators/WaveFunctionCollapse/Prototype.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Prototype")]
+public class Prototype : ScriptableObject
+{
+    public int meshRotation;
+    public GameObject prefab;
+    public WFC_Socket posX;
+    public WFC_Socket negX;
+    public WFC_Socket posZ;
+    public WFC_Socket negZ;
+    public List<Attribute> attributes = new List<Attribute>();
+    public NeighbourList validNeighbours;
+
+    [System.Serializable]
+    public class NeighbourList
+    {
+        public List<Prototype> posX = new List<Prototype>();
+        public List<Prototype> posZ = new List<Prototype>();
+        public List<Prototype> negX = new List<Prototype>();
+        public List<Prototype> negZ = new List<Prototype>();
+
+    }
+
+    public enum WFC_Socket { Socket_RoadStraight, Socket_RoadCurve, Socket_Lake, Socket_GrassFlat, Socket_GrassBouncy  }
+
+    [System.Serializable]
+    public class SocketConnection
+    {
+        public WFC_Socket socket;
+        public List<WFC_Socket> validConnections;
+    }
+}
