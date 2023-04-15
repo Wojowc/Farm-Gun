d9da4634e132b7ff0b2aeaa7180ae716ba5f84e0
Wroblotron <wrobel.julka20@gmail.com>
2023-04-16 01:08:49 +0200

    no message

--- Farm&Gun/Assets/Scripts/FarmAnimalsMenager/MobGenerator.cs
+++ Farm&Gun/Assets/Scripts/FarmAnimalsMenager/MobGenerator.cs
@@ -10,9 +10,12 @@ using UnityEngine.ProBuilder.MeshOperations;
 public class MobGenerator : MonoBehaviour
 {
     //tablica na male i duze zwierzeta dodawanie obiektu po stworzeniu
-    public GameObject[] FarmMobs;//[1-kura,2-kaczka] TODO
+    public GameObject[] FarmMob;//indes: [0-kura,1-kaczka,2-owca,3-swinia,4-krowa]
     [SerializeField]
     int[] alreadySpawned = { 0, 0, 0, 0, 0 };
+
+    [SerializeField]
+    private List<GameObject> FarmMobs;
 
     public Vector3 spawnValues; //values used to constraint? spawning region TODO
     int[] firstDice = { 0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 3, 4 };//first dice array
@@ -25,18 +28,23 @@ public class MobGenerator : MonoBehaviour
 
     void Start()
     {
-        //search for spawn points and ad correct nametags
+        //search for spawn points
         foundSpawnPoints = GameObject.FindGameObjectsWithTag("FarmAnimalsSpawnPoint");
-
     }
+
     void SpawnAnimal(int animalIndexNumber)
     {
-        Vector3 SpawnPoint = foundSpawnPoints[animalIndexNumber].transform.position;
-        float distance = maxAnimalAmount[animalIndexNumber] % 13 / 2;
-        float randomX = UnityEngine.Random.Range(-distance, distance);
+        Vector3 SpawnPoint = foundSpawnPoints[animalIndexNumber].transform.position; //get the correct animal spawnpoint
+        float distance = maxAnimalAmount[animalIndexNumber] % 13 / 2; //create a range depending on animal type
+        float randomX = UnityEngine.Random.Range(-distance, distance); //get random destination within the range for X and Z
         float randomZ = UnityEngine.Random.Range(-distance, distance);
-        Vector3 randomizedSpawnPosition = new Vector3(SpawnPoint.x + randomX, SpawnPoint.y, SpawnPoint.z + randomZ);
-        Instantiate(FarmMobs[animalIndexNumber], randomizedSpawnPosition, Quaternion.identity);
+        Vector3 randomizedSpawnPosition = new Vector3(SpawnPoint.x + randomX, SpawnPoint.y, SpawnPoint.z + randomZ); //create the random vector for spawning point
+       
+
+        GameObject farmMobInstance = GameObject.Instantiate(FarmMobs[animalIndexNumber], randomizedSpawnPosition, Quaternion.identity);
+        alreadySpawned[animalIndexNumber] += 1; //increase the alreadySpawned animals value with the rolled animals
+        FarmMobs.Add(farmMobInstance);
+        
     }
 
     void Update()
@@ -56,12 +64,12 @@ public class MobGenerator : MonoBehaviour
             {
                 //compare with animals with this index currently on farm
                 int animalsNewSum = (2 + alreadySpawned[firstAnimal]) / 2;
-                alreadySpawned[firstAnimal] += animalsNewSum; //increase the alreadySpawned animals value with the rolled animals
-                
+                       
                 for (int i = 0; i < animalsNewSum; i++)
                 {
                     if (alreadySpawned[firstAnimal] < maxAnimalAmount[firstAnimal])
                         SpawnAnimal(firstAnimal);
+                    
                     else
                         UnityEngine.Debug.Log("Przekroczono limit tego zwierzaka :< ");
             }
@@ -71,7 +79,6 @@ public class MobGenerator : MonoBehaviour
             else if (firstAnimal != secondAnimal)//if both rolled different
             {
                 int animalsNewSum = (1 + alreadySpawned[firstAnimal]) / 2;
-                alreadySpawned[firstAnimal] += animalsNewSum;
 
                 for (int i = 0; i < animalsNewSum; i++)
                 {
@@ -82,7 +89,6 @@ public class MobGenerator : MonoBehaviour
                 }
 
                 animalsNewSum = (1 + alreadySpawned[secondAnimal]) / 2;
-                alreadySpawned[secondAnimal] += animalsNewSum;
 
                 for (int i = 0; i < animalsNewSum; i++)
                 {
