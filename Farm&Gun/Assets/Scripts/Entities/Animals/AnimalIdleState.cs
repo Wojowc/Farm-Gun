56ff6c957d70dd10c9a9c1f57c20e3893cd06b11
Szymon Maciąg <maciagsz01@gmail.com>
2023-05-04 20:31:57 +0200

    sync with develop

---
+++ Farm&Gun/Assets/Scripts/Entities/Animals/AnimalIdleState.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalIdleState : State
+{
+    public override State RunCurrentState()
+    {
+        return this;
+    }
+}
