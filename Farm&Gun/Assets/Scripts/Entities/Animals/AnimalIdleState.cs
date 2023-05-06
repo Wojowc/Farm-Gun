a23c39a849845c7655bb8d854b7f7583af5cf96a
Szymon Maciąg <maciagsz01@gmail.com>
2023-05-06 19:59:22 +0200

    Working on idle state.

--- Farm&Gun/Assets/Scripts/Entities/Animals/AnimalIdleState.cs
+++ Farm&Gun/Assets/Scripts/Entities/Animals/AnimalIdleState.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Unity.IO.LowLevel.Unsafe;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Rendering.UI;
 
 public class AnimalIdleState : State
 {
     [SerializeField] private AnimalFollowPlayerState followPlayerState;
     [SerializeField] private NavMeshAgent navMesh;
     [SerializeField] private GameObject player;
+    private bool isIdleCoroutineRunning = false;
 
     public void Awake()
     {
@@ -19,11 +22,33 @@ public class AnimalIdleState : State
     {
         if (Vector3.Distance(player.transform.position, transform.position) > 8f)
         {
-
             Debug.Log("Enter follow player state");
+            if (isIdleCoroutineRunning)
+            {
+                StopCoroutine(IdleWalk());
+                isIdleCoroutineRunning = false;
+            }
             return followPlayerState;
         }
 
+        if (!isIdleCoroutineRunning)
+        {
+            Debug.Log("pyk0");
+            StartCoroutine(IdleWalk());
+            Debug.Log("pyk1");
+        }
         return this;
+    }
+
+    IEnumerator IdleWalk()
+    {
+        isIdleCoroutineRunning = true;
+        Vector3 randomDirection = Random.insideUnitSphere * 3;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randomDirection, out hit, 3, NavMesh.AllAreas);
+        navMesh.SetDestination(hit.position);
+        Debug.Log("Pyk");
+        yield return new WaitForSeconds(2f);
     }
 }
