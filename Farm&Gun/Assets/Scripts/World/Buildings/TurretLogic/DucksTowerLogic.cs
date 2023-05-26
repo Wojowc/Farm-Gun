06b7238745f764abbd3aaa007c499298f344fb33
dm\maciagsz <maciagsz01@gmail.com>
2023-05-26 13:24:19 +0200

    Healing fixes

--- Farm&Gun/Assets/Scripts/World/Buildings/TurretLogic/DucksTowerLogic.cs
+++ Farm&Gun/Assets/Scripts/World/Buildings/TurretLogic/DucksTowerLogic.cs
@@ -29,13 +29,14 @@ public class DucksTowerLogic : MonoBehaviour
         {
             yield return null;
         }
+
         foreach (GameObject go in animalList)
         {
             if (Vector3.Distance(go.transform.position, transform.position) < healingArea)
             {
                 var hm = go.GetComponent<HealthManager>();
                 if (hm != null)
-                    hm.Heal(5);
+                    hm.Heal(healingAmount);
             }
         }
 
